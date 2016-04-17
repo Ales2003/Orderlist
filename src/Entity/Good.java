0ea08badf424d9322896d2b373b56b9bd1e68b6f
Ales2003 <ales_2003@mail.ru>
2016-04-18 00:09:34 +0300

    version 0.1

---
+++ src/Entity/Good.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Алекс1
+ */
+public class Good {
+    private Integer idOfGood;
+    private String nameOfGood;
+    private Integer priceOfGood;
+    private String describtionOfGood;
+    
+    
+    
+}
---
+++ src/Entity/GoodsInOrder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Алекс1
+ */
+public class GoodsInOrder {
+    private Integer goodsInOrderId;
+    private Good goodInOrder;
+    private Integer quantityOfGoods;
+    
+    
+}
